44646a19f9533668186ab8f187aac2e3d266d76a
Inga Beck <ingambeck@gmail.com>
2026-03-27 10:10:37 +0000

    final cart test added

--- Inga_lausnir/lesson8/src/features/cart/cart.test.ts
+++ Inga_lausnir/lesson8/src/features/cart/cart.test.ts
@@ -1,21 +1,14 @@
 // test("1 + 1 equals 2", () => {
 //   expect(1 + 1).toBe(2);
 // });
-import { renderHook, act, render } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 import { useAppStore } from "@/shared/store/appStore";
 import type { Product } from "../products";
 
-import { CartSummary } from "./components/CartSummary";
-
 const mockProduct: Product = { id: "1", name: "test", price: 10 };
-const mockProductAnother: Product = { id: "2", name: "test2", price: 20 };
-const mockItems = [
-  {
-    mockProduct,
-    quantity: 2,
-  },
-  { mockProductAnother, quantity: 3 },
-];
+function setup() {
+  return renderHook(() => useAppStore());
+}
 
 //er ekki með cart provider í mínu...
 test("Add Product to set quantity 1", () => {
@@ -32,6 +25,9 @@ test("Add same Product to set quantity 2", () => {
   const { result } = renderHook(() => useAppStore(), {});
   act(() => {
     result.current.removeItem(mockProduct.id);
+  });
+  expect(result.current.items.length).toBe(0);
+  act(() => {
     result.current.addToCart(mockProduct);
     result.current.addToCart(mockProduct);
   });
@@ -49,8 +45,25 @@ test("Remove item, cart is empty", () => {
 });
 
 // Update quantity to 0 → item is removed SAME AS ONE BEFORE??
-// Cart total calculates correctly
-// test("cart total calculates correctly", () => {
-//   render
 
-// });
+test("Cart total calculates correctly", () => {
+  const mockProductAnother: Product = { id: "2", name: "test2", price: 5 };
+  const { result } = setup();
+
+  act(() => {
+    result.current.addToCart(mockProduct);
+  }); //price 10, gty 1
+  act(() => {
+    result.current.addToCart(mockProductAnother);
+  }); //price 5, gty 1
+  act(() => {
+    result.current.addToCart(mockProduct);
+  }); //price 10, gty 2
+
+  const total = result.current.items.reduce(
+    (sum: number, item: { product: { price: number }; quantity: number }) =>
+      sum + item.product.price * item.quantity,
+    0,
+  );
+  expect(total).toBe(25); //10*2 + 5*1
+});
