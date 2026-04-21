785ace10dd69ad1cc2bb4944271aa288cdb620cb
Inga Beck <ingambeck@gmail.com>
2026-04-21 20:05:01 +0000

    counter, greeint and math tests passed

--- lesson13/src/components/Greeting.test.tsx
+++ lesson13/src/components/Greeting.test.tsx
@@ -1,1 +1,34 @@
-describe('Greeting', () => {});
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import { Greeting } from "./Greeting";
+
+describe("Greeting", () => {
+  const user = userEvent.setup();
+
+  it("renders input with the correct placeholder", () => {
+    render(<Greeting />);
+    const inputElement = screen.getByPlaceholderText(/Skrifaðu nafn/i);
+    expect(inputElement).toBeInTheDocument;
+  });
+
+  it("allows user to type text in input", async () => {
+    render(<Greeting />);
+    const inputElement = screen.getByPlaceholderText(/Skrifaðu nafn/i);
+    await user.type(inputElement, "Ingzenegger");
+    expect(inputElement).toHaveValue("Ingzenegger");
+  });
+
+  it("greets user on submit", async () => {
+    render(<Greeting />);
+    const inputElement = screen.getByPlaceholderText(/Skrifaðu nafn/i);
+    await user.type(inputElement, "Ingzenegger");
+    expect(inputElement).toHaveValue("Ingzenegger");
+    const button = screen.getByRole("button", { name: /Senda/i });
+    await user.click(button);
+    expect(
+      screen.getByText((content, element) => {
+        return element?.textContent === "Halló, Ingzenegger!";
+      }),
+    ).toBeInTheDocument;
+  });
+});
--- lesson13/src/lib/math.test.ts
+++ lesson13/src/lib/math.test.ts
@@ -1,1 +1,11 @@
-describe('add', () => {});
+import { add } from "./math";
+
+describe("add", () => {
+  it("adds two numbers", () => {
+    expect(add(1, 2)).toBe(3);
+  });
+
+  it("can deal with negative numbers", () => {
+    expect(add(-1, 1)).toBe(0);
+  });
+});
